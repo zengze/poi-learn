209b7e68083506101ef349403210801788b62577
zengze <zengze@smart>
2017-07-12 14:51:27 +0800

    动态处理数据

--- src/main/java/happy/module/home/controller/HomeController.java
+++ src/main/java/happy/module/home/controller/HomeController.java
@@ -3,6 +3,7 @@ package happy.module.home.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import happy.module.home.dao.Home;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -16,9 +17,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.*;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @Controller
@@ -39,9 +43,8 @@ public class HomeController {
 
     @RequestMapping(value = "/export", method = { RequestMethod.GET, RequestMethod.POST })
     @ResponseBody
-    public void exportExcel(HttpServletRequest request,
-                            HttpServletResponse response,
-                            HttpSession session) throws IOException {
+    public void exportExcel(HttpServletRequest request, HttpServletResponse response, HttpSession session)
+            throws IOException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
 
         String year = request.getParameter("year");
         String data = request.getParameter("data");
@@ -62,17 +65,44 @@ public class HomeController {
                     + new String(fileName.getBytes("UTF-8"), "iso-8859-1"));
             outStream = response.getOutputStream();
 
+            // 从模板创建excel
             XSSFWorkbook wb = new XSSFWorkbook(instream);
+            // 获取模板的第一个sheet
             XSSFSheet sheet = wb.getSheetAt(0);
             int index = 0; //行
+            // 遍历数据
             for(Home home: excelList) {
+                // 新建行
                 XSSFRow row = sheet.createRow(++index);
-                for(int i = 0; i < 3; i++) {
-                    row.createCell(i);
+                // 获取实体类的所有属性，返回Field数组
+                Field[] field = home.getClass().getDeclaredFields();
+                for (int i = 0; i < field.length; i++) {
+                    // 新建列
+                    XSSFCell cell = row.createCell(i);
+                    // 获取属性的名字
+                    String fieldName = field[i].getName();
+                    // 取得getter方法名称
+                    String getMethodName = "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+                    // 创建gettet方法
+                    Method getMethod = home.getClass().getMethod(getMethodName);
+                    // 调用getter方法获取属性值
+                    Object value = getMethod.invoke(home);
+                    //判断值的类型后进行强制类型转换
+                    String textValue = null;
+                    if(value != null) {
+                        //其它数据类型都当作字符串简单处理
+                        textValue = value.toString();
+                        //利用正则表达式判断textValue是否全部由数字组成
+                        Pattern p = Pattern.compile("^//d+(//.//d+)?$");
+                        Matcher matcher = p.matcher(textValue);
+                        if(matcher.matches()){
+                            //是数字当作double处理
+                            cell.setCellValue(Double.parseDouble(textValue));
+                        } else {
+                            cell.setCellValue(textValue);
+                        }
+                    }
                 }
-                row.getCell(0).setCellValue(home.getName());
-                row.getCell(1).setCellValue(home.getAge());
-                row.getCell(2).setCellValue(home.getAddress());
             }
             wb.write(outStream);
         } finally {
